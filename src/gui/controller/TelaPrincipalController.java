58f46efa70483002e1257728cf8eb56aa7fb2264
EvertonTenorio <everton.tenorio10@gmail.com>
2018-08-19 17:05:58 -0300

    tela login linkada

--- src/gui/controller/TelaPrincipalController.java
+++ src/gui/controller/TelaPrincipalController.java
@@ -1,10 +1,36 @@
 package gui.controller;
 
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 public class TelaPrincipalController implements Initializable {
+
+    @FXML
+    private MenuItem btLogar;
+
+    @FXML
+    public void abrirTelaLogin() {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/gui/view/TelaLogin.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
