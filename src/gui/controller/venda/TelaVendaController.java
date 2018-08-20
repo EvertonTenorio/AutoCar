e14e9fdeaa66d9f1fcea76edc8ae7a9e626e86cd
EvertonTenorio <everton.tenorio10@gmail.com>
2018-08-20 17:35:12 -0300

    venda

--- src/gui/controller/venda/TelaVendaController.java
+++ src/gui/controller/venda/TelaVendaController.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -15,8 +16,11 @@ import javafx.scene.control.TextField;
 import javax.swing.JOptionPane;
 import negocio.Fachada;
 import negocio.entidade.Cliente;
+import negocio.entidade.Mecanico;
+import negocio.entidade.Pessoa;
 import negocio.entidade.Produto;
 import negocio.entidade.Servico;
+import negocio.entidade.ServicoPeriodico;
 import negocio.entidade.Venda;
 import negocio.execao.cliente.ClienteNaoExisteException;
 
@@ -32,15 +36,29 @@ public class TelaVendaController implements Initializable {
     private ComboBox<Produto> produtos = new ComboBox<>();
     @FXML
     private ComboBox<Servico> servicos = new ComboBox<>();
-    private Venda venda;
+    @FXML
+    private ComboBox<Mecanico> mecanicos = new ComboBox<>();
+    private Venda venda = null;
+    private Servico servico;
     @FXML
     private Label labelNome;
     @FXML
+    private Label mecanico;
+    @FXML
+    private Label kmAtual;
+    @FXML
+    private Label kmProx;
+    @FXML
+    private TextField txtkmAtual;
+    @FXML
+    private TextField txtkmProx;
+    @FXML
     private TextField txtCpf;
-    private Cliente cliente;
+    private Pessoa cliente;
     private Produto produto;
     @FXML
     private ListView<Produto> lista = new ListView<>();
+
     @FXML
     protected void preencherProdutos() {
 
@@ -57,9 +75,14 @@ public class TelaVendaController implements Initializable {
             if (!txtCpf.getText().equals("")) {
                 cliente = Fachada.getnstance().buscarCliente(txtCpf.getText());
                 labelNome.setText(cliente.getNome());
-
+                venda = new Venda(cliente);
             } else {
-                JOptionPane.showMessageDialog(null, "Cliente não identificado.");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Login");
+                alert.setHeaderText(null);
+                alert.setContentText("Cliente não identificado");
+
+                alert.showAndWait();
             }
 
         } catch (ClienteNaoExisteException e) {
@@ -67,10 +90,61 @@ public class TelaVendaController implements Initializable {
         }
 
     }
-    
+
     @FXML
-    protected void adicionarProduto(){
+    protected void adicionarProduto() {
+        produto = produtos.getValue();
+        venda.adicionarProduto(produto);
+    }
+
+    private void preencherMecanicos() {
+        ObservableList<Produto> listaMecanicos = FXCollections.observableArrayList(((ArrayList) Fachada.getnstance());
+        mecanicos.setItems(listaMecanicos);
+    }
+
+    @FXML
+    protected void selecionarServico() {
+        servico = servicos.getValue();
+
+        if (servico instanceof ServicoPeriodico) {
+            kmAtual.setVisible(true);
+            kmProx.setVisible(true);
+            txtkmAtual.setVisible(true);
+            txtkmProx.setVisible(true);
+        }
+        mecanicos.setVisible(true);
+        mecanico.setVisible(true);
+    }
+
+    @FXML
+    protected void adicionarServico() {
+        ServicoPeriodico s;
+        if (servico instanceof ServicoPeriodico) {
+            s = (ServicoPeriodico) servico;
+            try {
+                int km1 = Integer.parseInt(txtkmAtual.getText());
+                int km2 = Integer.parseInt(txtkmProx.getText());
+                s.setKmAtual(km1);
+                s.setKmProximoServico(km2);
+                if (mecanicos.getValue() != null) {
+                    s.setMecanico(mecanicos.getValue());
+                    venda.adicionarServico(servico, cliente.getCarros().get(0).getPlaca(), mecanicos.getValue());
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Venda");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Selecione o mecanico!");
+
+                    alert.showAndWait();
+                }
+            } catch (NumberFormatException e) {
+
+            }
+        }
         
+        servico.setMecanico(mecanicos.getValue());
+        
+        venda.adicionarServico(servico, cliente.getCarros().get(0).getPlaca(), mecanicos.getValue());
     }
 
     @Override
