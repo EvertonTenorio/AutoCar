bde76641de6ac422eeb9c79bdb7ef89449a9e818
JenilsinhoMoraes <jenilsinhomores@gmail.com>
2018-08-20 14:42:29 -0300

    Tela servico pronta

--- src/gui/controller/servico/TelaAlterarServicoController.java
+++ src/gui/controller/servico/TelaAlterarServicoController.java
@@ -2,13 +2,67 @@ package gui.controller.servico;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+import negocio.Fachada;
+import negocio.entidade.Servico;
+import negocio.execao.servico.ServicoNaoExisteException;
 
 public class TelaAlterarServicoController implements Initializable {
 
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtCodigo;
+    @FXML
+    private TextField txtValor;
+    private Servico servico;
+
+    @FXML
+    protected void buscarAlterarServico() {
+        try {
+            if (!txtCodigo.getText().equals("")) {
+                int codigo = Integer.parseInt(txtCodigo.getText());
+                servico = Fachada.getnstance().buscarServico(codigo);
+                txtNome.setText(servico.getNome());
+                txtValor.setText(servico.getValor() + "");
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Serviço não identificado.");
+
+            }
+        } catch (ServicoNaoExisteException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    @FXML
+    protected void alterar() {
+        try {
+            if (servico != null) {
+
+                String nome = txtNome.getText();
+                double valor = Double.parseDouble(txtValor.getText());
+                servico.setNome(nome);
+                servico.setValor(valor);
+
+                Fachada.getnstance().alterarServico(servico);
+                JOptionPane.showMessageDialog(null, "Serviço alterado.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Serviço não identificado.");
+            }
+        } catch (ServicoNaoExisteException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+    }
+
     @Override
+
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
--- src/gui/controller/servico/TelaRemoverServicoController.java
+++ src/gui/controller/servico/TelaRemoverServicoController.java
@@ -1,27 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui.controller.servico;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import negocio.Fachada;
+import negocio.entidade.Servico;
+import negocio.execao.servico.ServicoInvalidoException;
+import negocio.execao.servico.ServicoNaoExisteException;
 
-/**
- * FXML Controller class
- *
- * @author jenil
- */
 public class TelaRemoverServicoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField txtCodigo;
+    @FXML
+    private Label nome;
+    @FXML
+    private Label valor;
+    private Servico servico;
+
+    @FXML
+    protected void buscarRemoverServico() {
+
+        try {
+            if (txtCodigo.getText().equals("")) {
+                throw new ServicoInvalidoException();
+            } else {
+                int codigo = Integer.parseInt(txtCodigo.getText());
+                servico = Fachada.getnstance().buscarServico(codigo);
+
+                nome.setText(servico.getNome());
+                valor.setText(servico.getValor() + "");
+
+            }
+        } catch (ServicoNaoExisteException | ServicoInvalidoException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Remover");
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+
+            alert.showAndWait();
+        }
+
+    }
+
+    @FXML
+    protected void remover() {
+        try {
+            if (servico != null) {
+                Fachada.getnstance().removerServico(servico.getCodigo());
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Remover");
+                alert.setHeaderText(null);
+                alert.setContentText("Serviço Removido!");
+                nome.setText("");
+                valor.setText("");
+
+                alert.showAndWait();
+
+            } else {
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Remover");
+                alert.setHeaderText(null);
+                alert.setContentText("Serviço não encontrado");
+
+                alert.showAndWait();
+            }
+        } catch (ServicoNaoExisteException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Remover");
+            alert.setHeaderText(null);
+            alert.setContentText(e.getMessage());
+
+            alert.showAndWait();
+        }
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb
+    ) {
         // TODO
-    }    
-    
+    }
+
 }
