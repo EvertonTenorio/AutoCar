237354332fe195267a768f9a75cf51c4a4d619ec
EvertonTenorio <everton.tenorio10@gmail.com>
2019-06-07 17:23:08 -0300

    alteração na Conection

--- src/conection/ConectionFactory.java
+++ src/conection/ConectionFactory.java
@@ -26,7 +26,9 @@ public class ConectionFactory {
     }
     
     public static Connection getConnection(){
-        Conectar();
+        if (con == null)
+            Conectar();
+        
         return con;
     }
 }
